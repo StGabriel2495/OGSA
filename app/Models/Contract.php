1d102d3974900e270837c963da663de271a27859
romy-n <64855244+romy-n@users.noreply.github.com>
2023-11-10 19:19:11 +0100

    Table contact

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -19,7 +19,41 @@ class contract extends Model
         'path',
         'size',
         'signaturedate',
-        
-        
-    ];
+        ];
+
+        /**
+     * Get the comments for the blog post.
+     */
+    public function sector(): HasMany
+    {
+        return $this->hasMany(Sector::class);
+    }
+    public function contracttype(): HasMany
+    {
+        return $this->hasMany(Contracttype::class);
+    }
+    public function Project(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
+    public function illicitepractice(): HasMany
+    {
+        return $this->hasMany(Illicitepractice::class);
+    }
+    public function documenttype(): HasMany
+    {
+        return $this->hasMany(Documenttype::class);
+    }
+    public function stateentite(): HasMany
+    {
+        return $this->hasMany(Stateentite::class);
+    }
+    public function comment(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+    public function ressource(): HasMany
+    {
+        return $this->hasMany(Ressource::class);
+    }
 }
